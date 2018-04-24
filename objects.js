360fce9ae165f521648000de46bb78d03eb223c2
Lucy Mitchell <lucymhodges@gmail.com>
2018-04-24 12:25:31 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,8 @@
+var playlist = {
+  converge: 'homewrecker'
+  hit the lights: 'bodybag'
+  taylor swift: 'back to december'
+  beyonce: 'pray you catch me'
+};
+
+function updatePlaylist(playlist, [])
